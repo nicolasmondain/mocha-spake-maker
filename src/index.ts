5e574f4c64b46bdf976a8c719ad3d27d9652729c
Nicolas Mondain <nicolas.mondain@gmail.com>
2025-04-29 23:33:45 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,75 @@
 import { Runner, Test, reporters } from 'mocha';
-const { Markdown } = reporters;
+const { Base } = reporters;
 
 import { SPACE_MAKER_TAG } from './constants';
 
-class SpakeMakerReporter extends Markdown {
+class SpakeMakerReporter extends Base {
   private _indents: number = 0;
+  private _specs: Record<
+    string,
+    {
+      title: string;
+      file: string;
+      path: string;
+      specs: Array<string>;
+    }
+  > = {};
 
   constructor(runner: Runner) {
     super(runner);
     const stats = runner.stats || { passes: 0, failures: 0 };
 
     runner
-      .once(Runner.constants.EVENT_RUN_BEGIN, () => {
+      .once(Runner.constants.EVENT_RUN_BEGIN, (): void => {
         console.log(':>> start');
       })
       .on(
         Runner.constants.EVENT_SUITE_BEGIN,
-        (suite: Mocha.Suite & { [SPACE_MAKER_TAG]?: boolean }) => {
-          console.log(':>> ', SPACE_MAKER_TAG, suite);
-          if (suite[SPACE_MAKER_TAG]) {
-            console.log(':>> ', suite.title);
+        (suite: Mocha.Suite & { [SPACE_MAKER_TAG]?: boolean }): void => {
+          // @todo test with an array of suites
+          const suites: Array<Mocha.Suite & { [SPACE_MAKER_TAG]?: boolean }> = suite.suites;
+          for (const s of suites) {
+            if (s[SPACE_MAKER_TAG]) {
+              const { file, title } = s;
+              if (file && title) {
+                const split = file.split('/');
+                const ut = split.pop() ?? '';
+                this._specs[title] = {
+                  title,
+                  file: ut,
+                  path: `${split.join('/')}/`,
+                  specs: [],
+                };
+              }
+            }
           }
           this.increaseIndent();
         }
       )
       .on(
         Runner.constants.EVENT_SUITE_END,
-        (suite: Mocha.Suite & { [SPACE_MAKER_TAG]?: boolean }) => {
-          if (suite[SPACE_MAKER_TAG]) {
-            console.log(suite.title);
-          }
+        (suite: Mocha.Suite & { [SPACE_MAKER_TAG]?: boolean }): void => {
           this.decreaseIndent();
         }
       )
-      .on(Runner.constants.EVENT_TEST_PASS, (test: Test) => {
+      .on(Runner.constants.EVENT_TEST_PASS, (test: Test): void => {
         // Test#fullTitle() returns the suite name(s)
         // prepended to the test title
-        console.log(`${this.indent()}pass: ${test.fullTitle()}`);
+        const title = test.parent?.title ?? '';
+        if (this._specs[title]) {
+          this._specs[title].specs.push(`✅ ${test.fullTitle()}`);
+        }
       })
-      .on(Runner.constants.EVENT_TEST_FAIL, (test: Test, err: Error) => {
-        console.log(`${this.indent()}fail: ${test.fullTitle()} - error: ${err.message}`);
+      // @notes original args .on(Runner.constants.EVENT_TEST_FAIL, (test: Test, err: Error): void => {
+      .on(Runner.constants.EVENT_TEST_FAIL, (test: Test, err: Error): void => {
+        const title = test.parent?.title ?? '';
+        if (this._specs[title]) {
+          this._specs[title].specs.push(`❌ ${test.fullTitle()}`);
+        }
       })
-      .once(Runner.constants.EVENT_RUN_END, () => {
+      .once(Runner.constants.EVENT_RUN_END, (): void => {
         console.log(`end: ${stats.passes}/${stats.passes + stats.failures} ok`);
+        console.log(this._specs);
       });
   }
 
